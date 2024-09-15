4fec949a24eff4f0aafbb51cd50fa4f807fd2f0f
zelle105 <zelle105@umn.edu>
2024-09-14 21:10:46 -0500

    Updates to resume and projects.

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,8 +1,9 @@
 const About = () => {
     return (
       <div className="about">
-          <p>I'm Owen, a software/firmware dev and current student at the University of Minnesota Twin Cities.
-                I have experience building fullstack apps, writing embedded firmware for automative applications, and constructing machine learning models.</p>
+          <p>I'm Owen, a software developer and current student at the University of Minnesota Twin Cities.
+                I have experience building fullstack apps, writing embedded firmware for automative applications, and constructing machine learning models.
+                In my free time, I love fishing, gaming, and reading!</p>
       </div>
     )
   }
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,13 @@
+import Link from 'next/link';
+
 export const Projects = () => {
     return (
-      <div>page</div>
+      <div className="section-1">
+        <h1><Link href="https://github.com/Kasaar/KPress">KPress</Link></h1>
+        <h1><Link href="https://github.com/Kasaar/ProveML">ProveML</Link></h1>
+        <h1><Link href="https://github.com/Kasaar/zcrawler">zcrawler</Link></h1>
+        <h1><Link href="https://hub.docker.com/r/zeller7/drone_sim">DroneSim</Link></h1>
+      </div>
     )
   }
   
